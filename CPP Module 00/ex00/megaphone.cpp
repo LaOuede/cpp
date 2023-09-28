1de5c54b54ca8068b85a4d4abffda8cad86a5a7a
Gwenola Le Roux <gwenola.leroux@gmail.com>
2023-09-28 17:15:43 -0400

    minor changes in Mod00-ex00

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+using std::endl;
+
 int main(int argc, char **argv) {
 
 	if (argc == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
 		return 0 ;
 	}
 
 	for (int arg = 1; arg < argc; arg++) {
 		std::string str = argv[arg];
 		for (std::string::size_type i = 0; i < str.length(); i++) {
-			std::cout << (char)toupper(str[i]);
+			cout << (char)toupper(str[i]);
 		}
 	}
 	
-	std::cout << std::endl;
+	cout << endl;
 	return 0;
 }
