a00f1e93f5a6105217266e275c50f7db306bf766
Gwenola Le Roux <gwenola.leroux@gmail.com>
2023-10-05 16:33:55 -0400

    minor changes to Makefile after evaluation feedback

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -9,15 +9,14 @@ int main(int argc, char **argv) {
 	if (argc == 1) {
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
 		return 0 ;
-	}
-
-	for (int arg = 1; arg < argc; arg++) {
-		std::string str = argv[arg];
-		for (std::string::size_type i = 0; i < str.length(); i++) {
-			cout << (char)std::toupper(str[i]);
+	} else {
+		for (int arg = 1; arg < argc; arg++) {
+			std::string str = argv[arg];
+			for (std::string::size_type i = 0; i < str.length(); i++) {
+				cout << (char)std::toupper(str[i]);
+			}
 		}
 	}
-
 	cout << endl;
 	return 0;
 }
